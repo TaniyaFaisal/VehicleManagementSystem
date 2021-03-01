d432a229dc6c888bfd433c10b7b5e57dd0bedb2d
Taniya Faisal <taniyafaisal304@gmail.com>
2021-03-01 20:59:12 +0530

    commit changes

---
+++ VehicleManagementApp-SpringBoot/src/test/java/com/capg/VehicleManagement/VehicleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.VehicleManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
