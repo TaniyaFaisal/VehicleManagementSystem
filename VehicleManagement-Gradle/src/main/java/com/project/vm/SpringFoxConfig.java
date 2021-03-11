14e54acbc9dc186d48344b3a6c2222612eed3b4b
Taniya Faisal <taniyafaisal304@gmail.com>
2021-03-11 10:00:12 +0530

    Spring security added

--- VehicleManagement-Gradle/src/main/java/com/project/vm/SpringFoxConfig.java
+++ VehicleManagement-Gradle/src/main/java/com/project/vm/SpringFoxConfig.java
@@ -1,5 +1,7 @@
 package com.project.vm;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -7,6 +9,7 @@ import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,7 +24,20 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(getApiInfo());
     }
 
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Vehicle Management API",
+                null,
+                "VERSION 1",
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList()
+        );
+    }
 }
--- VehicleManagement-Gradle/src/test/java/com/project/vm/tests/BookingTest.java
+++ VehicleManagement-Gradle/src/test/java/com/project/vm/tests/BookingTest.java
@@ -1,6 +1,7 @@
 package com.project.vm.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,6 +19,7 @@ import com.project.vm.entities.Driver;
 import com.project.vm.entities.Vehicle;
 import com.project.vm.exceptions.DeletionException;
 import com.project.vm.exceptions.NotFoundException;
+import com.project.vm.exceptions.ValidationException;
 import com.project.vm.services.BookingService;
 
 @SpringBootTest
@@ -37,85 +40,142 @@ class BookingTest {
 		booking = new Booking();
 	}
 	
-//	@Test
+
+	@Test
 	void testAddBooking() {
+		customer.setFirstName("Abhi");
+		customer.setLastName("Sharma");
+		vehicle.setVehicleNumber("KA053277");
+		booking = new Booking(customer, vehicle,LocalDate.now(), LocalDate.of(2021, 03, 11), "Booking for one day", 200);
+		Booking b= bookingService.addBooking(booking);
+		assertTrue(b != null);
+	}
+	
+	@Test
+	void testAddBookingWithInvalidDates() {
 		customer.setFirstName("Test");
 		customer.setLastName("Cust");
 		vehicle.setVehicleNumber("AF786023");
 		booking = new Booking(customer, vehicle,LocalDate.of(2021, 03, 9), LocalDate.of(2021, 03, 10), "Booking for one day", 200);
-		Booking b= bookingService.addBooking(booking);
-		System.out.println(b);
+		Exception exception = assertThrows(ValidationException.class, () -> {
+			bookingService.addBooking(booking);
+	    });
+		String expectedMessage = "before current date";
+	    String actualMessage = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 
-//	@Test
+	@Test
 	void testViewAllBookings() {
 		List<Booking> bookings = bookingService.viewAllBookings();
 		System.out.println(bookings);
 		assertEquals(5, bookings.size());
 	}
 
-//	@Test
+	@Test
 	void testDeleteBooking() {
-		int id = 8;
-		bookingService.cancelBooking(id);
+		int id = 6;
+		Booking b = bookingService.cancelBooking(id);
+		assertTrue(b != null);
 	}
 	
-//	@Test
+	@Test
 	void testDeleteInvalidBooking() {
 		int id = 0;
 		Exception exception = assertThrows(NotFoundException.class, () -> {
-			bookingService.cancelBooking(id);;
+			bookingService.cancelBooking(id);
 	    });
 	    String expectedMessage = "not found";
 	    String actualMessage = exception.getMessage();
 	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 	
-//	@Test
+	@Test
 	void testDeleteBookingWithPayment() {
 		int id = 3;
 		Exception exception = assertThrows(DeletionException.class, () -> {
-			bookingService.cancelBooking(id);;
+			bookingService.cancelBooking(id);
 	    });
 	    String expectedMessage = "cannot be deleted";
 	    String actualMessage = exception.getMessage();
 	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 
-//	@Test
+	@Test
 	void testViewBooking() {
-		Booking b = bookingService.viewBooking(3);
+		Booking b = bookingService.viewBooking(4);
 		System.out.println(b);
+		assertNotNull(b);
+	}
+	
+	@Test
+	void testViewBookingByInvalidId() {
+		int id = 0;
+		Exception exception = assertThrows(NotFoundException.class, () -> {
+			bookingService.viewBooking(id);
+	    });
+	    String expectedMessage = "not found";
+	    String actualMessage = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 
-//	@Test
+	@Test
 	void testUpdateBooking() {
-		customer.setFirstName("Test");
-		customer.setLastName("Cust");
-		vehicle.setVehicleNumber("AF786023");
-		booking = new Booking(customer, vehicle,LocalDate.of(2021, 03, 9), LocalDate.of(2021, 03, 11), "Booking for two days", 200);	
+		booking.setBookingId(3);
+		booking.setBookedTillDate(LocalDate.of(2021, 03, 13));
+		booking.setBookingDescription("Booking for 3 days");
 		Booking b = bookingService.updateBooking(booking);
-		assertEquals("Booking for two days", b.getBookingDescription());
-		assertEquals(LocalDate.of(2021, 03, 11), b.getBookedTillDate());
-		System.out.println(b);
+		assertEquals("Booking for 3 days", b.getBookingDescription());
+		assertEquals(LocalDate.of(2021, 03, 13), b.getBookedTillDate());
 	}
 
-//	@Test
+	@Test
 	void testViewBookingByCustomer() {
 		List<Booking> bookings = bookingService.viewAllBooking("Priya");
-		System.out.println(bookings);
+		assertNotNull(bookings);
+	}
+	
+	@Test
+	void testViewBookingByInvalidCustomer() {
+		Exception exception = assertThrows(NotFoundException.class, () -> {
+			bookingService.viewAllBooking("xxxxx");
+	    });
+	    String expectedMessage = "No bookings found";
+	    String actualMessage = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 
-//	@Test
+	@Test
 	void testViewBookingByVehicle() {
-		List<Booking> bookings = bookingService.viewAllBookingByVehicle("AF786023");
-		System.out.println(bookings);
+		List<Booking> bookings = bookingService.viewAllBookingByVehicle("KA053279");
+		assertNotNull(bookings);
 	}
 
-//	@Test
+	
+	@Test
+	void testViewBookingByInvalidVehicle() {
+		Exception exception = assertThrows(NotFoundException.class, () -> {
+			bookingService.viewAllBookingByVehicle("xxxxx");
+	    });
+	    String expectedMessage = "not found";
+	    String actualMessage = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
+	}
+
+	@Test
 	void testViewBookingByBookingDate() {
 		List<Booking> bookings = bookingService.viewAllBookingByDate(LocalDate.of(2021, 03, 9));
-		System.out.println(bookings);
+		assertNotNull(bookings);
+	}
+	
+	@Test
+	void testViewBookingByInvalidDate() {
+		Exception exception = assertThrows(NotFoundException.class, () -> {
+			bookingService.viewAllBookingByDate(LocalDate.of(2000, 03, 01));
+	    });
+	    String expectedMessage = "No bookings found";
+	    String actualMessage = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 
 }
